a34bd725b75cf1c2d46f6fb416a0b3cefd4ff5d7
mrkezii <david.kezi@yahoo.com>
2016-03-10 19:53:33 +0300

    added card view  to the main layout

--- app/src/main/java/com/scurrae/chris/tangazo/Main.java
+++ app/src/main/java/com/scurrae/chris/tangazo/Main.java
@@ -1,13 +1,31 @@
 package com.scurrae.chris.tangazo;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import it.gmariotti.cardslib.library.internal.Card;
+import it.gmariotti.cardslib.library.internal.CardHeader;
+import it.gmariotti.cardslib.library.view.CardViewNative;
+
 public class Main extends AppCompatActivity {
+
+    private Activity activity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        Card card = new Card(getBaseContext());
+
+        //Create a CardHeader
+        CardHeader header = new CardHeader(getBaseContext());
+
+        //Add Header to card
+        card.addCardHeader(header);
+        CardViewNative cardView = (CardViewNative) Main.this.findViewById(R.id.carddemo);
+        cardView.setCard(card);
     }
+
+
 }
